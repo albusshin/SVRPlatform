beab3948a87d20035ab3e7ac629b4df18dc9a698
povergoing <povergoing@gmail.com>
2012-11-24 14:51:52 +0800

    Signed-off-by: povergoing <povergoing@gmail.com>

---
+++ src/Junit/test/Jingxuan/TestForHibernate.java
@@ -0,0 +1,24 @@
+package Junit.test.Jingxuan;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestForHibernate {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
