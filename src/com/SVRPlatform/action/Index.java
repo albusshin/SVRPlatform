143f63313495599e8dd7e86967a3850cd8a99694
owenxu10 <owenxu10@yahoo.com.cn>
2012-11-27 21:23:23 +0800

    修改了index 和 checkcookie 加了session

--- src/com/SVRPlatform/action/Index.java
+++ src/com/SVRPlatform/action/Index.java
@@ -1,11 +1,11 @@
 package com.SVRPlatform.action;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import com.SVRPlatform.service.LoginService;
-import com.SVRPlatform.util.CheckCookie;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class Index extends ActionSupport implements ServletRequestAware,ServletResponseAware{
@@ -13,18 +13,52 @@ public class Index extends ActionSupport implements ServletRequestAware,ServletR
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private LoginService loginService;
 	private HttpServletRequest request;  
-	
-	public void setLoginService(LoginService loginService) {
-		this.loginService = loginService;
-	}
+	private String cookieEmail = null;	
+	private String cookiePassword = null;
+	private LoginService loginService;
+
 	
 	public String execute()
 	{
-		System.out.println("InINDEX");
-		System.out.println(CheckCookie.check(request,loginService));
-		return CheckCookie.check(request,loginService);
+		System.out.println("In Index");
+		Cookie[] cookies = request.getCookies();		
+		System.out.println("getcookie");
+        if (cookies != null) {
+          for (int i = 0; i < cookies.length; i++) {									
+            System.out.println(cookies[i].getName() +"/////");
+            System.out.println(cookies[i].getValue() );
+        	  if (cookies[i].getName().equals("email")) {
+        		  cookieEmail= cookies[i].getValue();
+            }
+             
+        	  if (cookies[i].getName().equals("password")) {
+                  cookiePassword = cookies[i].getValue();
+            }
+          }
+        }
+       
+
+        if ((cookieEmail== null)&&(cookiePassword==null)) {
+          return "CookieNotFound";
+        }
+        else
+        {
+            System.out.println(cookieEmail);
+            System.out.println(cookiePassword);
+            boolean info = this.loginService.login(cookieEmail, cookiePassword);
+            if(!info)
+    		{	
+    			return "NotAuthenticated";
+    		}
+    		System.out.println("execute come on");
+        	return "LoggedIn";
+        }
+	}
+
+
+	public void setLoginService(LoginService loginService) {
+		this.loginService = loginService;
 	}
 
 
--- src/com/SVRPlatform/util/CheckCookie.java
+++ src/com/SVRPlatform/util/CheckCookie.java
@@ -2,7 +2,7 @@ package com.SVRPlatform.util;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import com.SVRPlatform.service.LoginService;
+import com.opensymphony.xwork2.ActionContext;
 
 public class CheckCookie {
 	
@@ -12,19 +12,21 @@ public class CheckCookie {
 	private static final long serialVersionUID = 1L;
 
 
-	public static String check(HttpServletRequest request,LoginService loginService)
+	public static String check(HttpServletRequest request)								//For redirection Check email & password  
 	{
-		String cookieEmail = null;
+		String cookieEmail = null;	
 		String cookiePassword = null;
-		Cookie[] cookies = null;	
+		String sessionEmail = null;
+		String sessionPassword = null;
+		boolean info = false;
+		
+		
 		System.out.println("checking");
-		cookies= request.getCookies();	
+		Cookie[] cookies = request.getCookies();		
 		System.out.println("getcookie");
         if (cookies != null) {
-        	
-          for (int i = 0; i < cookies.length; i++) {
+          for (int i = 0; i < cookies.length; i++) {									
             System.out.println(cookies[i].getName() +"/////");
-            
             System.out.println(cookies[i].getValue() );
         	  if (cookies[i].getName().equals("email")) {
         		  cookieEmail= cookies[i].getValue();
@@ -44,9 +46,16 @@ public class CheckCookie {
         {
             System.out.println(cookieEmail);
             System.out.println(cookiePassword);
-    		boolean info =loginService.login(cookieEmail, cookiePassword);
-    		System.out.println("---------------------");
-    		System.out.println(info);
+
+            sessionEmail = (String) request.getAttribute("email");
+            sessionPassword = (String) request.getAttribute("password");
+            
+            System.out.println(sessionEmail);
+            System.out.println(sessionPassword);
+            
+            if(sessionEmail.equals(cookieEmail) && sessionPassword.equals(cookiePassword))			
+            	info=true;
+            
     		if(!info)
     		{	
     			return "NotAuthenticated";
