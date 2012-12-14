9215a3efc0dcf54af1b3097aca7971d003bd151c
albusshin <albusshin@gmail.com>
2012-12-15 00:21:46 +0800

    上传截图的Action端 改名

--- src/com/SVRPlatform/action/UploadGraph.java
+++ src/com/SVRPlatform/action/UploadGraph.java
@@ -4,13 +4,22 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.core.ApplicationContext;
 import org.apache.commons.io.FileUtils;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 
 import com.SVRPlatform.constants.Constants;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.config.entities.ActionConfig;
 
-public class UploadGraph extends ActionSupport {
+public class UploadGraph extends ActionSupport  implements ServletRequestAware,		//sign up~ register
+ServletResponseAware {
 
 	/**
 	 * 
@@ -22,6 +31,8 @@ public class UploadGraph extends ActionSupport {
 	private String uploadPath;
 	private String message;
 	private InputStream inputStream;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
 
 	public InputStream getInputStream() {
 		return inputStream;
@@ -65,6 +76,11 @@ public class UploadGraph extends ActionSupport {
 				"/upload");
 		System.out.println(uploadPath);
 		if (graph != null) {
+			int indexOfDot = graphFileName.indexOf('.');
+			String prefix = "Temp";
+			String suffix = graphFileName.substring(indexOfDot);
+			System.out.println(request.getSession().getId() + suffix);
+			graphFileName = prefix + request.getSession().getId() + suffix;
 			File savefile = new File(new File(uploadPath), graphFileName);
 			if (!savefile.getParentFile().exists())
 				savefile.getParentFile().mkdirs();
@@ -90,5 +106,17 @@ public class UploadGraph extends ActionSupport {
 			return Constants.FAIL;
 		}
 
+	}
+
+	@Override
+	public void setServletResponse(HttpServletResponse arg0) {
+		// TODO Auto-generated method stub
+		response = arg0;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest arg0) {
+		// TODO Auto-generated method stub
+		request = arg0;
 	}
 }
