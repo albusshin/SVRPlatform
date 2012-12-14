b2e863246c8e0b66e6fb3a2d7379d531ef07ae22
owenxu10 <owenxu10@yahoo.com.cn>
2012-12-14 15:10:56 +0800

    Make comment and give solution

---
+++ src/com/SVRPlatform/action/MakeComment.java
@@ -0,0 +1,48 @@
+package com.SVRPlatform.action;
+
+import com.SVRPlatform.constants.Constants;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MakeComment extends ActionSupport
+{
+	/*
+	 * Get title and text from page,check it is null or not.
+	 */
+	
+	public String commentssubmittext;
+	public String commentssubmittitle;
+	public String message;
+	
+	public String getCommentssubmittext() {
+		return commentssubmittext;
+	}
+
+	public void setCommentssubmittext(String commentssubmittext) {
+		this.commentssubmittext = commentssubmittext;
+	}
+
+	public String getCommentssubmittitle() {
+		return commentssubmittitle;
+	}
+
+	public void setCommentssubmittitle(String commentssubmittitle) {
+		this.commentssubmittitle = commentssubmittitle;
+	}
+
+	public String execute()
+	{
+		System.out.println( "comment title:" + commentssubmittitle );
+		System.out.println( "comment text:" + commentssubmittext );
+		
+	 /*
+	  *   wait for the function supplied by Qingwei to check whether title and text are valid or not.
+	  *	 
+	  *   message=this.MakeCommentService.makeComment(commentssubmittitle,commentssubmittext); 
+	  *   if(message.equal("CommentValid")) return Constants.SUCCESS;
+	  *   else return Constants.FAIL;
+	  *
+	  */		
+	    return Constants.SUCCESS;
+		
+	}
+}
