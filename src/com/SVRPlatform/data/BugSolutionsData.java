f37f5a5a1614347f8af121ed8e81b4bf2710da3d
Gokeii <21250546@qq.com>
2012-12-17 20:01:38 +0800

    SolutionData and BugSolutionsData filled Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/SVRPlatform/data/BugSolutionsData.java
+++ src/com/SVRPlatform/data/BugSolutionsData.java
@@ -1,5 +1,22 @@
 package com.SVRPlatform.data;
 
+import java.util.List;
+
 public class BugSolutionsData {
 
+	private int solutionCount;
+	private List<SolutionData> solutionsData;
+	
+	public int getSolutionCount() {
+		return solutionCount;
+	}
+	public void setSolutionCount(int solutionCount) {
+		this.solutionCount = solutionCount;
+	}
+	public List<SolutionData> getSolutionsData() {
+		return solutionsData;
+	}
+	public void setSolutionsData(List<SolutionData> solutionsData) {
+		this.solutionsData = solutionsData;
+	}
 }
--- src/com/SVRPlatform/data/SolutionData.java
+++ src/com/SVRPlatform/data/SolutionData.java
@@ -2,4 +2,54 @@ package com.SVRPlatform.data;
 
 public class SolutionData {
 
+	private String content;
+	private String datetime;
+	private String realname;
+	private String email;
+	private int credits;
+	private int up;
+	private int down;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	public int getUp() {
+		return up;
+	}
+	public void setUp(int up) {
+		this.up = up;
+	}
+	public int getDown() {
+		return down;
+	}
+	public void setDown(int down) {
+		this.down = down;
+	}
 }
