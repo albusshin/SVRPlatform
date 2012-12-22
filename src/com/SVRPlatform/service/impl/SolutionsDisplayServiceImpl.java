e50d66f8f44d186bd038e997a75dbf605f0da691
Gokeii <21250546@qq.com>
2012-12-22 21:53:35 +0800

    changed solution display service Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/SVRPlatform/service/impl/SolutionsDisplayServiceImpl.java
+++ src/com/SVRPlatform/service/impl/SolutionsDisplayServiceImpl.java
@@ -1,21 +1,28 @@
 package com.SVRPlatform.service.impl;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 
 import com.SVRPlatform.dao.BugDAO;
 import com.SVRPlatform.dao.SolutionDAO;
+import com.SVRPlatform.dao.SolutionVoteDAO;
+import com.SVRPlatform.dao.UserDAO;
 import com.SVRPlatform.data.BugSolutionsData;
 import com.SVRPlatform.data.SolutionData;
 import com.SVRPlatform.model.Bug;
 import com.SVRPlatform.model.Solution;
+import com.SVRPlatform.model.SolutionVote;
 import com.SVRPlatform.model.User;
 import com.SVRPlatform.service.SolutionsDisplayService;
 
 public class SolutionsDisplayServiceImpl implements SolutionsDisplayService {
 	private BugDAO bugDAO;
+	private UserDAO userDAO;
 	private SolutionDAO solutionDAO;
+	private SolutionVoteDAO solutionVoteDAO;
 	
 	public SolutionDAO getSolutionDAO() {
 		return solutionDAO;
@@ -33,12 +40,27 @@ public class SolutionsDisplayServiceImpl implements SolutionsDisplayService {
 		this.bugDAO = bugDAO;
 	}
 
+	public SolutionVoteDAO getSolutionVoteDAO() {
+		return solutionVoteDAO;
+	}
+
+	public void setSolutionVoteDAO(SolutionVoteDAO solutionVoteDAO) {
+		this.solutionVoteDAO = solutionVoteDAO;
+	}
+
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
 	public BugSolutionsData solutionsDisplayService(int userID, String bugNumber, int pageNumber, int solutionsPerPage) {
 		BugSolutionsData bugSolutionsData = new BugSolutionsData();
 		
 		int bugID = Integer.parseInt(bugNumber.split("-")[2]);
-		Bug bug = (Bug) bugDAO.getByID(bugID);
-		
+		Bug bug = (Bug) bugDAO.getByID(bugID);		
 		int count =  (int) solutionDAO.getCountFromOneBug(bug);
 		if (bug.getOfficialSolutionId() != -1) count--;
 
@@ -50,6 +72,13 @@ public class SolutionsDisplayServiceImpl implements SolutionsDisplayService {
 		List<Solution> solutions = solutionDAO.getByBugId(bug, fetchSize, firstResult);
 		List<SolutionData> solutionsData = new LinkedList<SolutionData>();
 
+		Map<Integer, Integer> voted = new HashMap<Integer, Integer>();
+		if (userID != -1) {
+			List<SolutionVote> solutionVotes = solutionVoteDAO.getSolutionIdFromSolutionList(solutions, (User)userDAO.getByID(userID));			
+			for (int i = 0;i < solutionVotes.size();i++)
+				voted.put(solutionVotes.get(i).getSolution().getSolutionId(), solutionVotes.get(i).getVoteFlag());
+		}
+		
 		ListIterator<Solution> it = solutions.listIterator();
 		Solution solution;
 		SolutionData solutionData;
@@ -57,9 +86,19 @@ public class SolutionsDisplayServiceImpl implements SolutionsDisplayService {
 		while(it.hasNext()) {
 			solution = it.next();
 			solutionData = new SolutionData();
-			user = solution.getUser();
-			
-			solutionData.setSolutionID(solution.getSolutionId());
+			user = solution.getUser();			
+
+			solutionData.setVotedUp(false);
+			solutionData.setVotedDown(false);
+			int solutionID = solution.getSolutionId();
+			if (voted.containsKey(solutionID)) {
+				if (voted.get(solutionID) == 1)
+					solutionData.setVotedUp(true);
+				else
+					solutionData.setVotedDown(true);
+			}
+				
+			solutionData.setSolutionID(solutionID);
 			solutionData.setContent(solution.getContent());
 			solutionData.setDatetime(solution.getDatetime().toString());
 			solutionData.setRealname(user.getRealName());
@@ -79,16 +118,27 @@ public class SolutionsDisplayServiceImpl implements SolutionsDisplayService {
 		return bugSolutionsData;
 	}
 
-	public SolutionData officialSolutionDisplayService(String bugNumber) {
+	public SolutionData officialSolutionDisplayService(int userID, String bugNumber) {
 		int bugID = Integer.parseInt(bugNumber.split("-")[2]);
 		Bug bug = (Bug) bugDAO.getByID(bugID);
+		
 		
 		if (bug.getOfficialSolutionId() != -1) {
 			int officialSolutionID = bug.getOfficialSolutionId();
 			SolutionData solutionData = new SolutionData();
 			Solution solution = (Solution) solutionDAO.getByID(officialSolutionID);
-			User user = solution.getUser();
-			
+			User user = solution.getUser();			
+
+			solutionData.setVotedUp(false);
+			solutionData.setVotedDown(false);
+			if (userID != -1) {
+				SolutionVote solutionVote = solutionVoteDAO.getByUserAndSolution((User)userDAO.getByID(userID), solution);
+				if (solutionVote.getVoteFlag() == 1)
+					solutionData.setVotedUp(true);
+				else
+					solutionData.setVotedDown(true);
+			}
+				
 			solutionData.setSolutionID(solution.getSolutionId());
 			solutionData.setContent(solution.getContent());
 			solutionData.setDatetime(solution.getDatetime().toString());
