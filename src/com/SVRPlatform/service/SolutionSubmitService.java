4ea0118eb207e7165b415edcc6edb84a341f16d9
povergoing <povergoing@gmail.com>
2012-12-25 19:12:47 +0800

    Solution 提交唯一性，Solution的作者修改 service层 Signed-off-by: povergoing <povergoing@gmail.com>

--- src/com/SVRPlatform/service/SolutionSubmitService.java
+++ src/com/SVRPlatform/service/SolutionSubmitService.java
@@ -4,4 +4,5 @@ import java.util.Map;
 
 public interface SolutionSubmitService {
 	public Map<String, String> solutionSubmit(String bugNumber, String email, String content);
+	public Map<String, String> solutionEdit(String bugNumber, String email, String content);
 }
