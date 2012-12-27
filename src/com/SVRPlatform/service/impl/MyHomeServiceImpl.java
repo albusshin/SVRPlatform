ed571e0b3812f54836bb7582879f21638331644e
Gokeii <21250546@qq.com>
2012-12-27 20:03:46 +0800

    Signed-off-by: Gokeii <21250546@qq.com>

---
+++ src/com/SVRPlatform/service/impl/MyHomeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.SVRPlatform.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.SVRPlatform.data.BugData;
+import com.SVRPlatform.data.SolutionData;
+import com.SVRPlatform.service.MyHomeService;
+
+public class MyHomeServiceImpl implements MyHomeService {
+
+	public List<BugData> getMyBugs(int userID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<Integer, List<BugData>> getWatchingBugs(int userID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<SolutionData> getMySolutions(int userID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
