90b6d07c90f6a4a0978e1efc2dc973f380dba7d1
Gokeii <21250546@qq.com>
2012-12-27 20:19:51 +0800

    SolutionData changed
    Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/SVRPlatform/service/impl/MyHomeServiceImpl.java
+++ src/com/SVRPlatform/service/impl/MyHomeServiceImpl.java
@@ -1,21 +1,61 @@
 package com.SVRPlatform.service.impl;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
+import com.SVRPlatform.dao.BugDAO;
+import com.SVRPlatform.dao.BugWatchDAO;
+import com.SVRPlatform.dao.CommentDAO;
+import com.SVRPlatform.dao.SolutionDAO;
+import com.SVRPlatform.dao.UserDAO;
 import com.SVRPlatform.data.BugData;
 import com.SVRPlatform.data.SolutionData;
+import com.SVRPlatform.model.Bug;
+import com.SVRPlatform.model.User;
 import com.SVRPlatform.service.MyHomeService;
 
 public class MyHomeServiceImpl implements MyHomeService {
+	private UserDAO userDAO;
+	private BugDAO bugDAO;
+	private BugWatchDAO bugWatchDAO;
+	private CommentDAO commentDAO;
+	private SolutionDAO solutionDAO;
+	
 
 	public List<BugData> getMyBugs(int userID) {
-		// TODO Auto-generated method stub
-		return null;
+		if (userID == -1 ) return null;
+		User user = (User) userDAO.getByID(userID);
+		List<Bug> bugs = bugDAO.getByUser(user);
+		List<BugData> bugsData = new LinkedList<BugData>();
+		
+		ListIterator<Bug> it = bugs.listIterator();
+		Bug bug;
+		BugData bugData;
+		while (it.hasNext()) {
+			bug = it.next();
+			bugData = new BugData();
+			
+			bugData.setHasOfficial(bug.getOfficialSolutionId() != null && bug.getOfficialSolutionId() != -1);
+			bugData.setHasBest(bug.getBestSolutionId() != null && bug.getBestSolutionId() != -1);
+			bugData.setBugNumber(bug.getBugNumber());
+			bugData.setCommentsCount((int)commentDAO.getCountFromOneBug(bug));
+			bugData.setDigest(bug.getBugDigest());
+			bugData.setPublishDate(bug.getDatetime().toString());
+			bugData.setSolutionsCount((int)solutionDAO.getCountFromOneBug(bug));
+			bugsData.add(bugData);
+		}
+		
+		return bugsData;
 	}
 
 	public Map<Integer, List<BugData>> getWatchingBugs(int userID) {
-		// TODO Auto-generated method stub
+		if (userID == -1 ) return null;
+		User user = (User) userDAO.getByID(userID);
+		List<Bug> bugs = bugWatchDAO.getByUser(user);
+		List<BugData> bugsData = new LinkedList<BugData>();
+		
 		return null;
 	}
 
