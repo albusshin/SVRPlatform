ea603be793b6d9f2bc83cf584209029cde13f454
Gokeii <21250546@qq.com>
2013-01-01 13:42:25 +0800

    add 3 method into my home service
    Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/SVRPlatform/service/MyHomeService.java
+++ src/com/SVRPlatform/service/MyHomeService.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import com.SVRPlatform.data.BugData;
+import com.SVRPlatform.data.ExploitData;
 import com.SVRPlatform.data.SolutionData;
+import com.SVRPlatform.data.VulnerabilityData;
 
 public interface MyHomeService {
 	
 	public List<BugData> getMyBugs(int userID);
 	public Map<Integer, List<BugData>> getWatchingBugs(int userID);
 	public List<SolutionData> getMySolutions(int userID);
+	
+	public List<VulnerabilityData> getMyVulnerabilitys(int userID);
+	public Map<Integer, List<VulnerabilityData>> getWatchingVulnerabilitys(int userID);
+	public List<ExploitData> getMyExploits(int userID);
 }
